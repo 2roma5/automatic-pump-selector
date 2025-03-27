6c9f640497b3a02566fa6f3d85ab3bd3801648ac
konor <emilioxkonor@gmail.com>
2025-03-27 15:55:14 -0600

    Migrate from Flask to FastAPI
    Templates and routing have been updated to use FastAPI conventions,
    dependencies have been updated in requirements.txt, and template
    references have been adjusted accordingly.

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-from flask import Flask, render_template
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from core.units import Q_, convert_units
 
-app = Flask(__name__)
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
-@app.route('/')
-def index():
+@app.get('/', response_class=HTMLResponse)
+def index(request: Request):
     flow = Q_(1, 'meter ** 3 / second')
     flow = convert_units(flow, 'gallon / minute')
-    return render_template("home.html", css_name="home.css", flow=flow)
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    return templates.TemplateResponse("home.html", {"request": request, "css": "home.css", "flow": flow})
